e85c12acee5200ae3c7a8adffb44f1bea2d07eaf
juaancamilo82 <juaancamilo82@outlook.com>
2023-09-01 15:57:16 -0500

    primer y segundo taller

---
+++ EAM_CNC/Taller2/script.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+variable=${1}
+
+mensajes() {
+echo "hackeando la nasa............."
+echo "accediendo a los sistema.........."
+echo "OBteniendo información bancaria..."
+echo ""
+echo "El numero ingresado es: "$variable
+echo "Stiven me lo mama" 
+}
+
+fecha() {
+echo "Obteniendo la fecha"
+date
+}
+
+
+echo ""
+
+mensajes
+fecha
